805f6c6c60e03afdf5521ba26c8284f5f05f4b5d
Vladimir Magamedov <vladimir@magamedov.com>
2019-05-06 18:18:54 +0300

    Switched to the yaml.safe_load

--- strictconf/yaml.py
+++ strictconf/yaml.py
@@ -11,5 +11,5 @@ def init(conf, file_names, variant, sep='.'):
     conf_data = {}
     for file_name in file_names:
         with codecs.open(file_name, encoding='utf-8') as f:
-            conf_data.update(yaml.load(f.read()))
+            conf_data.update(yaml.safe_load(f.read()))
     data.init(conf, conf_data, variant, sep=sep)
